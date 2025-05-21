da6cd06b08812477d9dd7d2bdc37795fa9c9f9f9
HyungJoo Yoon <67642494+yhj0904@users.noreply.github.com>
2025-05-21 13:44:25 +0900

    :memo: Update CorsConfig

--- src/main/java/com/nanwe/push/config/CorsMvcConfig.java
+++ src/main/java/com/nanwe/push/config/CorsMvcConfig.java
@@ -11,6 +11,8 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
 
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000", "http://localhost:8082")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
     }
 }
